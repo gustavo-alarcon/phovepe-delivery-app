7df51efdb60ae79b2d6217153286f887089013a2
Junjiro <jobando@meraki-s.com>
2020-06-08 11:02:26 -0500

    Auto stash before merge of "develop" and "origin/develop"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,17 @@
+<<<<<<< Updated upstream
 import { DatabaseService } from 'src/app/core/database.service';
 import { Component, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
+=======
+import { Component, Renderer2, ViewChild, ElementRef, Inject } from '@angular/core';
+import { Observable, combineLatest } from 'rxjs';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { tap, startWith, map, pairwise } from 'rxjs/operators'
+import { MaterialCssVarsService } from 'angular-material-css-vars';
+import { DOCUMENT } from '@angular/common';
+import { DatabaseService } from './core/database.service';
+>>>>>>> Stashed changes
  
 @Component({
   selector: 'app-root',
@@ -11,12 +21,26 @@ import { DOCUMENT } from '@angular/common';
 export class AppComponent {
 
   title = 'meraki-delivery-app';
+<<<<<<< Updated upstream
   constructor(
     @Inject(DOCUMENT) private _document: HTMLDocument,
     private dbs: DatabaseService,
   ) {
     this.dbs.document = this._document
     
+=======
+
+  constructor(
+    @Inject(DOCUMENT) private _document: HTMLDocument,
+    private dbs: DatabaseService,
+    )
+    {
+      this.dbs.document = this._document
+    }
+
+
+  ngOnInit(){
+>>>>>>> Stashed changes
 
   }
 
