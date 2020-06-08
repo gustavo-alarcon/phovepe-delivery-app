329f989186a062f53ed60366d709ac0c11d2aa53
Junjiro <jobando@meraki-s.com>
2020-06-08 11:08:23 -0500

    lazy loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,3 @@
-<<<<<<< Updated upstream
-import { DatabaseService } from 'src/app/core/database.service';
-import { Component, Inject } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-
-=======
 import { Component, Renderer2, ViewChild, ElementRef, Inject } from '@angular/core';
 import { Observable, combineLatest } from 'rxjs';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
@@ -11,7 +5,6 @@ import { tap, startWith, map, pairwise } from 'rxjs/operators'
 import { MaterialCssVarsService } from 'angular-material-css-vars';
 import { DOCUMENT } from '@angular/common';
 import { DatabaseService } from './core/database.service';
->>>>>>> Stashed changes
  
 @Component({
   selector: 'app-root',
@@ -21,28 +14,11 @@ import { DatabaseService } from './core/database.service';
 export class AppComponent {
 
   title = 'meraki-delivery-app';
-<<<<<<< Updated upstream
   constructor(
     @Inject(DOCUMENT) private _document: HTMLDocument,
     private dbs: DatabaseService,
   ) {
     this.dbs.document = this._document
-    
-=======
-
-  constructor(
-    @Inject(DOCUMENT) private _document: HTMLDocument,
-    private dbs: DatabaseService,
-    )
-    {
-      this.dbs.document = this._document
-    }
-
-
-  ngOnInit(){
->>>>>>> Stashed changes
-
   }
-
 }
 
