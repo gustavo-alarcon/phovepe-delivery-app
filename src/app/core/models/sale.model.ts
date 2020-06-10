68f296732632e5c0a4ab8b7846dc4d6bb2e83f3c
Junjiro <jobando@meraki-s.com>
2020-06-10 11:38:35 -0500

    yap

--- src/app/core/models/sale.model.ts
+++ src/app/core/models/sale.model.ts
@@ -22,7 +22,11 @@ export interface Sale {
   productList: {
     product: Product,
     quantity: number
-  }[],
+
+    //Limite de bloqueo y solicitud (inventario-semaforo)
+  }[],//Solicitado, Atendido (se agrega info de vendedora, se puede despues de eso cambiar quesos, agregar foto), Confirmado, Biker en camino, entregado
+  //Cambiar foto -> solo por la de finanzans antes de confirmacion
+  //almacen: Incompleto y correcto
   status: string, //'Solicitado', 'Confirmado', 'En reparto', 'Entregado', 'Cancelado'
   total: number,
   deliveryPrice: number,
