d55e018533aabba960bb346d8adf6624e161ebca
MelanieOC <mocharan@meraki-s.com>
2020-06-12 18:22:40 -0500

    project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCVS7VLaRqFfzWPf88JVXZipwGgrGozQ7M",
-    authDomain: "meraki-delivery-app.firebaseapp.com",
-    databaseURL: "https://meraki-delivery-app.firebaseio.com",
-    projectId: "meraki-delivery-app",
-    storageBucket: "meraki-delivery-app.appspot.com",
-    messagingSenderId: "693057003451",
-    appId: "1:693057003451:web:326a498eeda56357492e9e"
-    /*
     apiKey: "AIzaSyDXFvxUQQt2NIa_Dr2sNjI_nQFwvZ4MEug",
     authDomain: "phovepe-delivery-app.firebaseapp.com",
     databaseURL: "https://phovepe-delivery-app.firebaseio.com",
@@ -16,6 +8,7 @@ export const environment = {
     storageBucket: "phovepe-delivery-app.appspot.com",
     messagingSenderId: "391819129574",
     appId: "1:391819129574:web:7ee5cf956179fed7ba8127",
-    measurementId: "G-XRERWX98RV"*/
+    measurementId: "G-XRERWX98RV"
+    
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,6 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCVS7VLaRqFfzWPf88JVXZipwGgrGozQ7M",
-    authDomain: "meraki-delivery-app.firebaseapp.com",
-    databaseURL: "https://meraki-delivery-app.firebaseio.com",
-    projectId: "meraki-delivery-app",
-    storageBucket: "meraki-delivery-app.appspot.com",
-    messagingSenderId: "693057003451",
-    appId: "1:693057003451:web:326a498eeda56357492e9e"
-    /*
     apiKey: "AIzaSyDXFvxUQQt2NIa_Dr2sNjI_nQFwvZ4MEug",
     authDomain: "phovepe-delivery-app.firebaseapp.com",
     databaseURL: "https://phovepe-delivery-app.firebaseio.com",
@@ -20,7 +12,8 @@ export const environment = {
     storageBucket: "phovepe-delivery-app.appspot.com",
     messagingSenderId: "391819129574",
     appId: "1:391819129574:web:7ee5cf956179fed7ba8127",
-    measurementId: "G-XRERWX98RV"*/
+    measurementId: "G-XRERWX98RV"
+   
   }
 };
 
